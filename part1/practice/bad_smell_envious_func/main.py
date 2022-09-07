4fc19f78a044987f99361c3ceb07e8f4e3ec7e63
Админ <lenzik_1991@mail.ru>
2022-09-07 21:45:03 +0500

    1

--- part1/practice/bad_smell_envious_func/main.py
+++ part1/practice/bad_smell_envious_func/main.py
@@ -2,7 +2,6 @@
 # чаще дергает методы класса Cube? Исправьте так, 
 # чтобы избавиться от лишних обращений к классу Cube
 
-
 class Cube:
 
     def __init__(self, x, y, z):
@@ -10,18 +9,35 @@ class Cube:
         self.y = y
         self.z = z
 
-    def get_x(self):
-        return self.x
-
-    def get_y(self):
-        return self.y
-
-    def get_z(self):
-        return self.z
+    def get_volume(self):
+        return self.x * self.y * self.z
 
 
 class CubeVolumeCalculator:
 
     @staticmethod
     def calc_cube_volume(cube):
-        return cube.get_x() * cube.get_y() * cube.get_z()
+        return cube.get_volume()
+
+# class Cube:
+#
+#     def __init__(self, x, y, z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#
+#     def get_x(self):
+#         return self.x
+#
+#     def get_y(self):
+#         return self.y
+#
+#     def get_z(self):
+#         return self.z
+#
+#
+# class CubeVolumeCalculator:
+#
+#     @staticmethod
+#     def calc_cube_volume(cube):
+#         return cube.get_x() * cube.get_y() * cube.get_z()
